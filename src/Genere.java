c4539e11b71f0e2774634e9aefcff268f454a41c
Emanuele <piccimanu@icloud.com>
2025-09-23 11:42:25 +0200

    Aggiunta classe Utente

---
+++ src/Genere.java
@@ -0,0 +1,3 @@
+public enum Genere {
+    Maschio, Femmina, Non_Definito;
+}
--- src/Utente.java
+++ src/Utente.java
@@ -1,3 +1,10 @@
-public class Utente {
+import java.util.Date;
 
+public class Utente {
+    private String nome;
+    private String cognome;
+    private String username;
+    private Date dataNascita;
+    private Genere genere;
+    private String password;
 }
