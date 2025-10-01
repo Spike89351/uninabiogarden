370cfde3a43942db52c0c7cfc309e386074cf9c0
Emanuele <piccimanu@icloud.com>
2025-10-01 13:50:48 +0200

    Aggiunta del costruttore, getters e setters della classe Terreno e aggiunta attributi classe Progetto

--- src/Progetto.java
+++ src/Progetto.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Progetto {
+    private String NomeProgetto;
+    private Date DataFine;
+    private Date DataInizio;
+    private String DescrizioneProgetto;
+    private ArrayList<Notifica> Notify = new ArrayList<Notifica>();
+    private Terreno Terreno;
+    private StatoProgetto StatoProgetto;
 }
--- src/Terreno.java
+++ src/Terreno.java
@@ -1,3 +1,71 @@
-public class Terreno {
+import java.util.ArrayList;
 
+public class Terreno {
+    private String ID_Terreno;
+    private Proprietario Prop;
+    private double superficie;
+    private TipoTerreno TipologiaTerreno;
+    private ArrayList<Progetto> Project = new ArrayList<Progetto>();
+    private ArrayList<TipoAttivita> TipoAttivita = new ArrayList<TipoAttivita>();
+    private ArrayList<String> StoricoColtura = new ArrayList<String>();
+
+
+    //COSTRUTTORE:
+    Terreno(String ID_Terreno, Proprietario Prop, double superficie, TipoTerreno TipologiaTerreno, ArrayList<Progetto> Project, ArrayList<TipoAttivita> TipoAttivita, ArrayList<String> StoricoColtura) {
+        this.ID_Terreno = ID_Terreno;
+        this.Prop = Prop;
+        this.superficie = superficie;
+        this.TipologiaTerreno = TipologiaTerreno;
+        this.Project = Project;
+        this.TipoAttivita = TipoAttivita;
+        this.StoricoColtura = StoricoColtura;
+    }
+
+    //GETTERS:
+
+    public String getID_Terreno() {
+        return ID_Terreno;
+    }
+    public Proprietario getProp() {
+        return Prop;
+    }
+    public double getSuperficie() {
+        return superficie;
+    }
+    public TipoTerreno getTipologiaTerreno() {
+        return TipologiaTerreno;
+    }
+    public ArrayList<Progetto> getProject() {
+        return Project;
+    }
+    public ArrayList<String> getStoricoColtura() {
+        return StoricoColtura;
+    }
+    public ArrayList<TipoAttivita> getTipoAttivita() {
+        return TipoAttivita;
+    }
+
+    //SETTERS:
+
+    public void setID_Terreno(String ID_Terreno) {
+        this.ID_Terreno = ID_Terreno;
+    }
+    public void setProp(Proprietario Prop) {
+        this.Prop = Prop;
+    }
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    public void setTipologiaTerreno(TipoTerreno TipologiaTerreno) {
+        this.TipologiaTerreno = TipologiaTerreno;
+    }
+    public void setProject(ArrayList<Progetto> Project) {
+        this.Project = Project;
+    }
+    public void setStoricoColtura(ArrayList<String> StoricoColtura) {
+        this.StoricoColtura = StoricoColtura;
+    }
+    public void setTipoAttivita(ArrayList<TipoAttivita> TipoAttivita) {
+        this.TipoAttivita = TipoAttivita;
+    }
 }
