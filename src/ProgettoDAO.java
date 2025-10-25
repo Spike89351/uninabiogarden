73828ae2dbf20ba1064dd4e364d0962dc852daf4
Maat11 <enzocapuo@gmail.com>
2025-10-25 18:38:18 +0200

    Creazione della classe ProgetoDAO

---
+++ src/ProgettoDAO.java
@@ -0,0 +1,11 @@
+
+public class ProgettoDAO {
+	private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "Informatica1";
+	
+	
+	//FUNZIONE CHE SERVE PER RISALIRE AI PROGETTI:
+	
+	
+}
