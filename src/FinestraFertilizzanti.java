e288a6194f4349795b81041fb344846e9921e0c4
Maat11 <enzocapuo@gmail.com>
2025-11-08 22:48:56 +0100

    aggiunta di un metodo nella pagina che controlli se i valori inseriti sono corretti

--- src/FinestraFertilizzanti.java
+++ src/FinestraFertilizzanti.java
@@ -16,12 +16,17 @@ import javax.swing.GroupLayout.Alignment;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
 
 public class FinestraFertilizzanti extends JDialog {
 	private Controller theController;
 	
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JTextField txtLetame;
+	private JTextField txtCompost;
+	private JTextField txtGranulari;
+	private JTextField txtLiquidi;
 
 	
 	public FinestraFertilizzanti(int idDep, Controller c) {
@@ -35,7 +40,7 @@ public class FinestraFertilizzanti extends JDialog {
 		});
 		
 		setTitle("Pagina Fertilizzanti");
-		setSize(450, 300);
+		setSize(450, 328);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,6 +69,18 @@ public class FinestraFertilizzanti extends JDialog {
 			lblGranulari.setFont(new Font("Tahoma", Font.PLAIN, 12));
 			JLabel lblLiquidi = new JLabel("Liquidi");
 			lblLiquidi.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			
+			txtLetame = new JTextField();
+			txtLetame.setColumns(10);
+			
+			txtCompost = new JTextField();
+			txtCompost.setColumns(10);
+			
+			txtGranulari = new JTextField();
+			txtGranulari.setColumns(10);
+			
+			txtLiquidi = new JTextField();
+			txtLiquidi.setColumns(10);
 			GroupLayout gl_panelCentral = new GroupLayout(panelCentral);
 			gl_panelCentral.setHorizontalGroup(
 				gl_panelCentral.createParallelGroup(Alignment.LEADING)
@@ -71,14 +88,26 @@ public class FinestraFertilizzanti extends JDialog {
 						.addGroup(gl_panelCentral.createParallelGroup(Alignment.LEADING)
 							.addGroup(gl_panelCentral.createSequentialGroup()
 								.addContainerGap()
-								.addGroup(gl_panelCentral.createParallelGroup(Alignment.LEADING)
-									.addComponent(lblMessaggioAttenzione, GroupLayout.DEFAULT_SIZE, 408, Short.MAX_VALUE)
-									.addComponent(lblLetame, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-									.addComponent(lblCompost, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-									.addComponent(lblGranulari, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)))
+								.addComponent(lblMessaggioAttenzione, GroupLayout.DEFAULT_SIZE, 408, Short.MAX_VALUE))
 							.addGroup(gl_panelCentral.createSequentialGroup()
-								.addGap(14)
-								.addComponent(lblLiquidi, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)))
+								.addGap(123)
+								.addGroup(gl_panelCentral.createParallelGroup(Alignment.LEADING)
+									.addGroup(gl_panelCentral.createSequentialGroup()
+										.addComponent(lblLetame, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+										.addGap(18)
+										.addComponent(txtLetame, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGroup(gl_panelCentral.createSequentialGroup()
+										.addComponent(lblCompost, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+										.addGap(18)
+										.addComponent(txtCompost, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGroup(gl_panelCentral.createSequentialGroup()
+										.addComponent(lblGranulari, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+										.addGap(18)
+										.addComponent(txtGranulari, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGroup(gl_panelCentral.createSequentialGroup()
+										.addComponent(lblLiquidi, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+										.addGap(18)
+										.addComponent(txtLiquidi, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))))
 						.addContainerGap())
 			);
 			gl_panelCentral.setVerticalGroup(
@@ -86,15 +115,23 @@ public class FinestraFertilizzanti extends JDialog {
 					.addGroup(gl_panelCentral.createSequentialGroup()
 						.addContainerGap()
 						.addComponent(lblMessaggioAttenzione)
+						.addGap(29)
+						.addGroup(gl_panelCentral.createParallelGroup(Alignment.BASELINE)
+							.addComponent(lblLetame)
+							.addComponent(txtLetame, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 						.addGap(18)
-						.addComponent(lblLetame)
+						.addGroup(gl_panelCentral.createParallelGroup(Alignment.BASELINE)
+							.addComponent(lblCompost, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+							.addComponent(txtCompost, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 						.addGap(18)
-						.addComponent(lblCompost, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_panelCentral.createParallelGroup(Alignment.BASELINE)
+							.addComponent(lblGranulari, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+							.addComponent(txtGranulari, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 						.addGap(18)
-						.addComponent(lblGranulari, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
-						.addGap(25)
-						.addComponent(lblLiquidi, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(51, Short.MAX_VALUE))
+						.addGroup(gl_panelCentral.createParallelGroup(Alignment.BASELINE)
+							.addComponent(lblLiquidi, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+							.addComponent(txtLiquidi, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addContainerGap(47, Short.MAX_VALUE))
 			);
 			panelCentral.setLayout(gl_panelCentral);
 		}
@@ -107,9 +144,15 @@ public class FinestraFertilizzanti extends JDialog {
 				btnAggiungi.setToolTipText("I fertilizzanti che inserirai verranno sommati a quelli già presenti");
 				btnAggiungi.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						//AGGIUNGI FERTILIZZANTE:
-						if(theController.inserisciFertilizzante(idDep, idDep, idDep, idDep, idDep)) {
-							JOptionPane.showMessageDialog(null, "L'inserimento dei fertilizzante è avvenuto con successo");
+						//CONTROLLO SE I DATI SONO STATI INSERITI CORRETTAMENTE:
+						if(ctrlFieldTxt()) {
+							//AGGIUNGI FERTILIZZANTE:
+							if(theController.inserisciFertilizzante(idDep, Double.valueOf(txtLetame.getText().trim()), Double.valueOf(txtCompost.getText().trim()), Double.valueOf(txtGranulari.getText().trim()), Double.valueOf(txtLiquidi.getText().trim()))) {
+								clearTextFields();
+								JOptionPane.showMessageDialog(null, "L'inserimento dei fertilizzante è avvenuto con successo");
+							}else {
+								JOptionPane.showMessageDialog(null, "Errore nell'inserimento del fertilizzante");
+							}
 						}
 					}
 				});
@@ -122,6 +165,9 @@ public class FinestraFertilizzanti extends JDialog {
 				btnBack.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						setVisible(false);
+						
+						//PULISCI I CAMPI NEL CASO CI FOSSE BISOGNO:
+						clearTextFields();
 					}
 				});
 				btnBack.setActionCommand("Cancel");
@@ -129,5 +175,50 @@ public class FinestraFertilizzanti extends JDialog {
 			}
 		}
 	}
-
+	
+//METODI:
+	//METODO CHE PULISCE I CAMPI DI TESTO:
+	private void clearTextFields() {
+		txtLetame.setText(null);
+		txtCompost.setText(null);
+		txtGranulari.setText(null);
+		txtLiquidi.setText(null);
+	}
+	
+	//CONTROLLA CAMPI DI TESTO:
+	private boolean ctrlFieldTxt() {
+		if(txtLetame.getText().isBlank()) {
+			txtLetame.setText("-1");
+		}else {
+			if(Double.valueOf(txtLetame.getText().trim()) <= 0) {
+				JOptionPane.showMessageDialog(null, "Errore ma il letame non può essere un valore minore o uguale a 0!");
+				return false;
+			}
+		}
+		if(txtCompost.getText().isBlank()) {
+			txtCompost.setText("-1");
+		}else {
+			if(Double.valueOf(txtCompost.getText().trim()) <= 0) {
+				JOptionPane.showMessageDialog(null, "Errore ma il compost non può avere un valore minore o uguale a 0!");
+				return false;
+			}
+		}
+		if(txtGranulari.getText().isBlank()) {
+			txtGranulari.setText("-1");
+		}else {
+			if(Double.valueOf(txtGranulari.getText().trim()) <= 0) {
+				JOptionPane.showMessageDialog(null, "Errore ma i granulari non possono avere un valore minore o uguale a 0!");
+				return false;
+			}
+		}
+		if(txtLiquidi.getText().isBlank()) {
+			txtLiquidi.setText("-1");
+		}else {
+			if(Double.valueOf(txtLiquidi.getText().trim()) <= 0) {
+				JOptionPane.showMessageDialog(null, "Errore ma i liquidi non può avere un valore minore o uguale a 0!");
+				return false;
+			}
+		}
+		return true;
+	}
 }
