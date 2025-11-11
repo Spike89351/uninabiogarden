73255986c27c9a7e4a0d877b8f74a4e7129f2346
Maat11 <enzocapuo@gmail.com>
2025-11-11 09:16:52 +0100

    inserimento delle getters e delle setters nella classe attività, costruttore incompleto;
    - aggiunta di elementi nella paginaAttività;

--- src/Attività.java
+++ src/Attività.java
@@ -12,6 +12,71 @@ public class Attività {
     private int quantitàRaccolto;
     private Fertilizzante fertilizzanteUsato;
     
+    Attività(Terreno ter){
+    	
+    }
+
+//GETTERS:
+	public ArrayList<Coltivatore> getElencoColt() {
+		return elencoColt;
+	}
+	public CondizioneRaccolto getCondizioneRaccolto() {
+		return condizioneRaccolto;
+	}
+	public Stato getStatoEsecuzione() {
+		return statoEsecuzione;
+	}
+	public Date getDataRaccolto() {
+		return dataRaccolto;
+	}
+	public Terreno getTerreno() {
+		return terreno;
+	}
+	public Deposito getDeposito() {
+		return deposito;
+	}
+	public int getQuantitàRaccoltoPrevisto() {
+		return quantitàRaccoltoPrevisto;
+	}
+	public int getQuantitàRaccolto() {
+		return quantitàRaccolto;
+	}
+	public Fertilizzante getFertilizzanteUsato() {
+		return fertilizzanteUsato;
+	}
+	
+	
+	
+	
+	
+//SETTERS:    
+	public void setElencoColt(ArrayList<Coltivatore> elencoColt) {
+		this.elencoColt = elencoColt;
+	}
+	public void setCondizioneRaccolto(CondizioneRaccolto condizioneRaccolto) {
+		this.condizioneRaccolto = condizioneRaccolto;
+	}
+	public void setStatoEsecuzione(Stato statoEsecuzione) {
+		this.statoEsecuzione = statoEsecuzione;
+	}
+	public void setDataRaccolto(Date dataRaccolto) {
+		this.dataRaccolto = dataRaccolto;
+	}
+	public void setTerreno(Terreno terreno) {
+		this.terreno = terreno;
+	}
+	public void setDeposito(Deposito deposito) {
+		this.deposito = deposito;
+	}
+	public void setQuantitàRaccoltoPrevisto(int quantitàRaccoltoPrevisto) {
+		this.quantitàRaccoltoPrevisto = quantitàRaccoltoPrevisto;
+	}
+	public void setQuantitàRaccolto(int quantitàRaccolto) {
+		this.quantitàRaccolto = quantitàRaccolto;
+	}
+	public void setFertilizzanteUsato(Fertilizzante fertilizzanteUsato) {
+		this.fertilizzanteUsato = fertilizzanteUsato;
+	}
     
     
 }
--- src/PaginaAttività.java
+++ src/PaginaAttività.java
@@ -3,15 +3,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class PaginaAttività extends JFrame {
 	private Controller theController;
 	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
+	private JPanel panelTop;
+	private JPanel panelCentral;
+	private JPanel panelBottom;
+	private JLabel lblWelcome;
 	
-	public PaginaAttività(Controller c) {
+	public PaginaAttività(int idTerreno, Controller c) {
 		theController = c;
 		
 		setTitle("Pagina attività");
@@ -21,7 +27,20 @@ public class PaginaAttività extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		panelTop = new JPanel();
+		contentPane.add(panelTop, BorderLayout.NORTH);
+		
+		lblWelcome = new JLabel("Cambia il tipo di attività sul terreno con id "+idTerreno);
+		lblWelcome.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panelTop.add(lblWelcome);
+		
+		panelCentral = new JPanel();
+		contentPane.add(panelCentral, BorderLayout.CENTER);
+		
+		panelBottom = new JPanel();
+		contentPane.add(panelBottom, BorderLayout.SOUTH);
 	}
 
 }
