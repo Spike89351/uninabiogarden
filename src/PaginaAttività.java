3787eff3a1e2d229341c0895b5cd3812e586cca0
Maat11 <enzocapuo@gmail.com>
2025-11-11 08:42:41 +0100

    - eliminazione della pagina TipoAttività e creata una nuova PaginaAttività

--- src/PaginaAttività.java
+++ src/PaginaAttività.java
@@ -4,17 +4,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class PaginaTipoAttività extends JFrame {
+public class PaginaAttività extends JFrame {
 	private Controller theController;
 	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	
-	public PaginaTipoAttività(Controller c) {
-		theController = c;		
+	public PaginaAttività(Controller c) {
+		theController = c;
 		
-		setTitle("Pagina per visualizzare il tipo di attività");
+		setTitle("Pagina attività");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(450, 300);
 		setLocationRelativeTo(null);
