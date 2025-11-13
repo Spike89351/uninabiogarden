2c82ff70437bbee3a1d0a6bc943f62a02106c440
Maat11 <enzocapuo@gmail.com>
2025-11-13 16:02:48 +0100

    miglioramento codice

--- src/Attrezzo.java
+++ src/Attrezzo.java
@@ -6,9 +6,7 @@ public class Attrezzo {
     private String nome;
     private int  idAttrezzo;
     private TipoAttrezzo tipo;
-    private ArrayList<Date> elencoManutenzioni = new ArrayList<Date>();
     private StatoAttrezzo statoAttrezzo;
-    private ArrayList<String> elencoManStraordinaria = new ArrayList<String>();
     private boolean disp;
 
     Attrezzo(String nome, TipoAttrezzo typ, StatoAttrezzo stato){
@@ -29,14 +27,8 @@ public class Attrezzo {
     public TipoAttrezzo getTipo() {
         return tipo;
     }
-    public ArrayList<Date> getElencoManutenzioni() {
-        return elencoManutenzioni;
-    }
     public StatoAttrezzo getStatoAttrezzo() {
         return statoAttrezzo;
-    }
-    public ArrayList<String> getElencoManStraordinaria() {
-        return elencoManStraordinaria;
     }
     public boolean isDisp() {
         return disp;
@@ -54,27 +46,8 @@ public class Attrezzo {
     public void setTipo(TipoAttrezzo tipo) {
         this.tipo = tipo;
     }
-    public void setElencoManutenzioni(Date Manutenzione) {
-        //MI SERVE PER CONTROLLARE SE ESISTE LA DATA:
-        boolean ctrl = true;
-        //CONTROLLO SE LA DATA GIA' E' ESISTENTE:
-        for(int i=0; i<elencoManutenzioni.size(); i++) {
-            if(elencoManutenzioni.get(i).equals(Manutenzione)) {
-                ctrl = false;
-            }
-        }
-        if(! ctrl) {
-            JOptionPane.showMessageDialog(null, "La data che stai inserendo è già presente nell'elenco della manutenzione dell'attrezzo!");
-        }else {
-            //ALTRIMENTI INSERISCI:
-            elencoManutenzioni.add(Manutenzione);
-        }
-    }
     public void setStatoAttrezzo(StatoAttrezzo statoAttrezzo) {
         this.statoAttrezzo = statoAttrezzo;
-    }
-    public void setElencoManStraordinaria(String manutenzioneStrao) {
-        elencoManStraordinaria.add(manutenzioneStrao);
     }
     public void setDisp(boolean disp) {
         this.disp = disp;
