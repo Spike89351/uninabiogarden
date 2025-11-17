ba33b498dcfccf9f3e061552dae4359316c56b48
Maat11 <enzocapuo@gmail.com>
2025-11-17 11:46:37 +0100

    Eliminazione delle tabelle

--- src/StoricoColtivatoreDAO.java
+++
@@ -1,7 +0,0 @@
-
-public class StoricoColtivatoreDAO {
-	private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-	private static final String USER = "postgres";
-	private static final String PASSWORD = "Informatica1";
-	
-}
