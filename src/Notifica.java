ef05cc83d5d09c840c2ee8433d3dad7afb03dd94
Maat11 <enzocapuo@gmail.com>
2025-11-28 10:49:07 +0100

    miglioramento codice

--- src/Notifica.java
+++ src/Notifica.java
@@ -1,15 +1,19 @@
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class Notifica {
 	
 	private int idNotifica;
 	private String descrizione;
+	private java.sql.Date dataCreazione;
 	private TipoNotifica tipoNotifica;
 	
 	
 	public Notifica(String descrizione, TipoNotifica tipoNotifica) {
+		LocalDate now = LocalDate.now();
 		this.descrizione = descrizione;
 		this.tipoNotifica = tipoNotifica;
+		this.dataCreazione = java.sql.Date.valueOf(now);
 	}
 	
 //GETTERS:
@@ -18,6 +22,9 @@ public class Notifica {
 	}
 	public String getDescrizione() {
 		return descrizione;
+	}
+	public java.sql.Date getDataCreazione() {
+		return dataCreazione;
 	}
 	public TipoNotifica getTipoNotifica() {
 		return tipoNotifica;
@@ -29,6 +36,9 @@ public class Notifica {
 	}
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
+	}
+	public void getTipoNotifica(java.sql.Date nowDat) {
+		 this.dataCreazione = nowDat;
 	}
 	public void setTipoNotifica(TipoNotifica tipoNotifica) {
 		this.tipoNotifica = tipoNotifica;
