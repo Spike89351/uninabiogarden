f96433f36c332be0dfcdd6c26d06ab2069e9763e
Maat11 <enzocapuo@gmail.com>
2026-04-10 14:48:28 +0200

    creazione della classe OperatoreDAO

---
+++ src/OperatoreDAO.java
@@ -0,0 +1,10 @@
+
+public class OperatoreDAO {
+
+	private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "Informatica1";
+	
+	
+	
+}
