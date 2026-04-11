d3f7178b37aa527e701c68db2d191391beafe952
Maat11 <enzocapuo@gmail.com>
2026-04-11 18:26:49 +0200

    modifiche

--- src/PaginaPrincipaleOperatore.java
+++ src/PaginaPrincipaleOperatore.java
@@ -3,24 +3,54 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class PaginaPrincipaleOperatore extends JFrame {
-	private Controller theController;
-	
+
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	
-	public PaginaPrincipaleOperatore(Controller c) {
-		theController = c;
-		
-		setTitle("Pagina Principale");
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PaginaPrincipaleOperatore frame = new PaginaPrincipaleOperatore();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PaginaPrincipaleOperatore() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setExtendedState(JFrame.MAXIMIZED_BOTH);//RENDE LA PAGINA A SCHERMO INTERO DIRETTAMENTE;
-		setLocationRelativeTo(null);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelTop = new JPanel();
+		contentPane.add(panelTop, BorderLayout.NORTH);
+		
+		JLabel lblWelcome = new JLabel("New label");
+		lblWelcome.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panelTop.add(lblWelcome);
+		
+		JPanel panelCentral = new JPanel();
+		contentPane.add(panelCentral, BorderLayout.CENTER);
+		
+		JPanel panelBottom = new JPanel();
+		contentPane.add(panelBottom, BorderLayout.SOUTH);
 
 	}
 
